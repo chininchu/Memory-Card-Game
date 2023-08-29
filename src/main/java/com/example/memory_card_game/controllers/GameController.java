357c94ea45eebf0cc8d4a7f1917c7e0ee1ec832e
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-29 17:32:23 -0500

    Created dependencies injection

--- src/main/java/com/example/memory_card_game/controllers/GameController.java
+++ src/main/java/com/example/memory_card_game/controllers/GameController.java
@@ -1,4 +1,26 @@
 package com.example.memory_card_game.controllers;
 
+import com.example.memory_card_game.Repository.PlayerRepository;
+import com.example.memory_card_game.Repository.ScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class GameController {
+
+
+    private final PlayerRepository playerRepository;
+
+    private final ScoreRepository scoreRepository;
+
+
+    public GameController(PlayerRepository playerRepository, ScoreRepository scoreRepository) {
+        this.playerRepository = playerRepository;
+        this.scoreRepository = scoreRepository;
+    }
+
+
+
+
+
 }
