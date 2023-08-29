cd4bc8184df9c279d21eb1ee0aad3951bf9fa3b2
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-29 17:40:29 -0500

    Created a displayGame method

--- src/main/java/com/example/memory_card_game/controllers/GameController.java
+++ src/main/java/com/example/memory_card_game/controllers/GameController.java
@@ -4,6 +4,8 @@ import com.example.memory_card_game.Repository.PlayerRepository;
 import com.example.memory_card_game.Repository.ScoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GameController {
@@ -19,6 +21,17 @@ public class GameController {
         this.scoreRepository = scoreRepository;
     }
 
+    @GetMapping("/game")
+
+    public String displayGame() {
+
+        // Logic to display the game
+        return "game";
+
+        // This refers to the game.html (or game template) file
+
+
+    }
 
 
 
