5380d1ce2a049b57a650a0dee593fd0faddfda8e
chininchu <120990876+chininchu@users.noreply.github.com>
2023-09-08 15:04:04 -0500

    Created a function that does a fetch request

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -2,9 +2,32 @@
 
 function toggleCard(card) {
 
+    const cardId = card.getAttribute("data-card-id");
+
     const isFlipped = card.getAttribute("data-flipped") === "true";
 
-    card.setAttribute("data-flipped", !isFlipped);
+    fetch(`/toggleCard?cardId=${cardId}`, {
+        method: 'POST',
+
+    })
+
+        .then(Response => Response.text())
+
+        .then((message) => {
+            console.log(message);
+
+            if (message === 'Card flipped') {
+                card.setAttribute("data-flipped", !isFlipped);
+
+            }
+
+
+        })
+
+        .catch((Error) => {
+            console.error('Error:', Error);
+
+        });
 
 
 }
