201f2f50092f5086104adf0853c7356d6c438061
chininchu <120990876+chininchu@users.noreply.github.com>
2023-09-12 18:23:29 -0500

    Our refactored API is working, but we need to now change the logic within the controller that will control the state of the cards

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,33 +1,45 @@
 "use strict";
 
-function toggleCard(card) {
+let firstCard = null;
 
+function cardClicked(card) {
+    // const card = card.target;
     const cardId = card.getAttribute("data-card-id");
 
-    const isFlipped = card.getAttribute("data-flipped") === "true";
-
-    fetch(`/toggleCard?cardId=${cardId}`, {
+    fetch(`http://localhost:8080/toggleCard?cardId=${cardId}`, {
         method: 'POST',
-
     })
+        .then(response => response.text())
+        .then((response) => {
+            console.log(response);  // Log the server response for debugging
 
-        .then(Response => Response.text())
+            // Toggle the flipped class to flip the card
+            card.classList.toggle('flipped');
 
-        .then((message) => {
-            console.log(message);
+            // Check if it's the first card being flipped
+            if (!firstCard) {
+                firstCard = card;  // Store the first card
+            } else {
+                // Check for a match with the second card
+                if (firstCard.getAttribute('data-icon-name') === card.getAttribute('data-icon-name')) {
+                    console.log('Match found!');
+                    // (Further actions for a match can be added here)
+                } else {
+                    console.log('No match');
+                    // (Further actions for a non-match can be added here)
 
-            if (message === 'Card flipped') {
-                card.setAttribute("data-flipped", String(!isFlipped));
+                    // Flip the cards back over after a delay
+                    setTimeout(() => {
+                        firstCard.classList.remove('flipped');
+                        card.classList.remove('flipped');
+                    }, 1000);
+                }
 
+                // Reset the first card variable for the next turn
+                firstCard = null;
             }
-
-
         })
-
-        .catch((Error) => {
-            console.error('Error:', Error);
-
+        .catch((error) => {
+            console.error('Error:', error);
         });
-
-
 }
