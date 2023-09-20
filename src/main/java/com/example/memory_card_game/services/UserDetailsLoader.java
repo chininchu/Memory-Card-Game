412dc3b161ac84c271411970d75399e77ac0a129
chininchu <120990876+chininchu@users.noreply.github.com>
2023-09-20 16:47:50 -0500

    Created the SecurityConfig class

--- src/main/java/com/example/memory_card_game/services/UserDetailsLoader.java
+++ src/main/java/com/example/memory_card_game/services/UserDetailsLoader.java
@@ -6,6 +6,10 @@ import com.example.memory_card_game.model.UserWithRoles;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
 
 public class UserDetailsLoader implements UserDetailsService {
 
