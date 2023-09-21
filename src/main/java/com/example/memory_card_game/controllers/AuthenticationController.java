457ba9dcaced8e70473ac4b2a5c97cba6ee86d17
chininchu <120990876+chininchu@users.noreply.github.com>
2023-09-21 16:24:23 -0500

    Receiving a 403 error

---
+++ src/main/java/com/example/memory_card_game/controllers/AuthenticationController.java
@@ -0,0 +1,16 @@
+package com.example.memory_card_game.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthenticationController {
+
+
+    @GetMapping("/login")
+
+    public String showLoginForm(){
+
+        return "/users/login";
+    }
+}
