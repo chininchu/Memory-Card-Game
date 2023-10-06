c3a6c4e85cfca4a3b4af07d53a15b48a96ae0f6b
chininchu <120990876+chininchu@users.noreply.github.com>
2023-10-06 17:52:13 -0500

    Added the sec:authorize to attribute in the game.html file

--- src/main/java/com/example/memory_card_game/controllers/AuthenticationController.java
+++ src/main/java/com/example/memory_card_game/controllers/AuthenticationController.java
@@ -1,10 +1,35 @@
 package com.example.memory_card_game.controllers;
 
+import com.example.memory_card_game.Repository.UserRepository;
+import com.example.memory_card_game.model.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
 
 @Controller
 public class AuthenticationController {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+
+    private AuthenticationController(UserRepository userRepository,PasswordEncoder passwordEncoder){
+
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+
+    }
 
 
     @GetMapping("/login")
@@ -12,5 +37,21 @@ public class AuthenticationController {
     public String showLoginForm(){
 
         return "/users/login";
+    }
+
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user, Model model, HttpServletRequest request) {
+        // Retrieve the user object from the database based on the provided username
+        User authenticatedUser = userRepository.findByUsername(user.getUsername());
+        System.out.println("Username:" + authenticatedUser);
+        // Check if the user exists and the password matches
+        if (authenticatedUser != null && passwordEncoder.matches(user.getPassword(), authenticatedUser.getPassword())) {
+            // Authentication successful, set the user attribute in the session
+            request.getSession().setAttribute("user", authenticatedUser);
+            return "redirect:/profile";
+        }
+        // if Authentication failed, redirect back to the login page with an error message
+        return "redirect:/login";
     }
 }
--- src/main/java/com/example/memory_card_game/controllers/UserController.java
+++ src/main/java/com/example/memory_card_game/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.example.memory_card_game.Repository.PlayerRepository;
 import com.example.memory_card_game.Repository.UserRepository;
 import com.example.memory_card_game.model.Player;
 import com.example.memory_card_game.model.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -57,6 +58,21 @@ public class UserController {
         return "redirect:/login";
 
     }
+
+//    @PostMapping("/LoginPage")
+//    public String loginUser(@ModelAttribute User user, Model model, HttpServletRequest request) {
+//        // Retrieve the user object from the database based on the provided username
+//        User authenticatedUser = userDao.findByUsername(user.getUsername());
+//        System.out.println("Username:" + authenticatedUser);
+//        // Check if the user exists and the password matches
+//        if (authenticatedUser != null && encoder.matches(user.getPassword(), authenticatedUser.getPassword())) {
+//            // Authentication successful, set the user attribute in the session
+//            request.getSession().setAttribute("user", authenticatedUser);
+//            return "redirect:/profile";
+//        }
+//        // if Authentication failed, redirect back to the login page with an error message
+//        return "redirect:LoginPage";
+//    }
 
 
 }
